fae5610bc6000af9f4d8b7bb65067e64ed1f1dc9
RIGLO <aidaouisedik28@gmail.com>
2026-02-04 06:56:19 +0100

    Update Sound.cs

--- All File C#/Muthode/Sound.cs
+++ All File C#/Muthode/Sound.cs
@@ -1,1 +1,21 @@
+  private bool soundMuted = false;
+  private void PlaySound(string soundFileName)
+  {
+      if (soundMuted)
+          return;
 
+      var assembly = Assembly.GetExecutingAssembly();
+      var resourceName = $"PlantillaChanchoV9.{soundFileName}";
+
+      using (Stream soundStream = assembly.GetManifestResourceStream(resourceName))
+      {
+          if (soundStream != null)
+          {
+              using (SoundPlayer player = new SoundPlayer(soundStream))
+              {
+                  player.Stop();   // يوقف أي صوت شغال
+                  player.Play();   // يشغل الجديد
+              }
+          }
+      }
+  }

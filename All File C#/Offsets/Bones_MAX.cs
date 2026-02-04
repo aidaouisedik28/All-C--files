7ca0171b4cd7637f0908adce0233fb745bac6f7d
RIGLO <aidaouisedik28@gmail.com>
2026-02-04 07:18:53 +0100

    Add files via upload

---
+++ All File C#/Offsets/Bones_MAX.cs
@@ -0,0 +1,25 @@
+// Free Fire MAX Bones
+namespace AotForms
+{
+    internal enum Bones_MAX : uint
+    {
+            Head = 0x3e8,
+            LeftWrist = 0x3e4,
+            Root = 0x3fc,
+            Spine = 0x3f0,
+            Hip = 0x3f8,
+            RightCalf = 0x400,
+            LeftCalf = 0x404,
+            RightFoot = 0x408,
+            LeftFoot = 0x40c,
+            RightWrist = 0x410,
+            LeftHand = 0x414,
+            LeftShoulder = 0x41c,
+            RightShoulder = 0x420,
+            RightWristJoint = 0x424,
+            LeftWristJoint = 0x428,
+            LeftElbow = 0x42c,
+            RightElbow = 0x430,
+        }
+    }
+
